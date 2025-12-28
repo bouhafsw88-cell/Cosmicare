9439a15500038bfcc3889364222d8cc6e5ec72f6
bouhafsw88-cell <bouhafsw88@gmail.com>
2025-12-28 11:54:47 +0100

    Add Navbar component with navigation links

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,1 +1,15 @@
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="nav-logo">Cosmicare</div>
 
+      <ul className="nav-links">
+        <li>Accueil</li>
+        <li>Visage</li>
+        <li>Corps</li>
+        <li>Cheveux</li>
+        <li>Promo</li>
+      </ul>
+    </nav>
+  );
+}
