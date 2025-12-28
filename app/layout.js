6c0ca7d1566bd27422be7cb8bc10f053cfb5f68a
bouhafsw88-cell <bouhafsw88@gmail.com>
2025-12-28 11:15:51 +0100

    Add global styles and metadata to layout

--- app/layout.js
+++ app/layout.js
@@ -1,1 +1,15 @@
 
+import "./globals.css";
+
+export const metadata = {
+  title: "Cosmicare",
+  description: "Boutique cosmétique en ligne",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="fr">
+      <body>{children}</body>
+    </html>
+  );
+}
