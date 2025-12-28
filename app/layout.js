8f7077b1afca2b7501588809aa8bef71d480a89e
bouhafsw88-cell <bouhafsw88@gmail.com>
2025-12-28 11:59:47 +0100

    Add Navbar component to RootLayout

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Navbar from "./components/Navbar";
 
 export const metadata = {
   title: "Cosmicare",
@@ -9,10 +10,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="fr">
       <body>
-        <header className="header">
-          <h1 className="logo">Cosmicare</h1>
-        </header>
-
+        <Navbar />
         <main>{children}</main>
 
         <footer className="footer">
