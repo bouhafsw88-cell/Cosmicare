e309774d1c6de1ff0969bd424b383b20d4a37242
bouhafsw88-cell <bouhafsw88@gmail.com>
2025-12-28 12:20:21 +0100

    Replace static nav items with Link components

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,14 +1,16 @@
+import Link from "next/link";
+
 export default function Navbar() {
   return (
     <nav className="navbar">
       <div className="nav-logo">Cosmicare</div>
 
       <ul className="nav-links">
-        <li>Accueil</li>
-        <li>Visage</li>
-        <li>Corps</li>
-        <li>Cheveux</li>
-        <li>Promo</li>
+        <li><Link href="/">Accueil</Link></li>
+        <li><Link href="/visage">Visage</Link></li>
+        <li><Link href="/corps">Corps</Link></li>
+        <li><Link href="/cheveux">Cheveux</Link></li>
+        <li><Link href="/promo">Promo</Link></li>
       </ul>
     </nav>
   );
