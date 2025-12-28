9ca34b7dc38df0590e933da311c26b1a39d0c8e0
bouhafsw88-cell <bouhafsw88@gmail.com>
2025-12-28 11:13:37 +0100

    Create Home component for Cosmicare landing page

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "20px" }}>
+      <h1>Bienvenue sur Cosmicare 💄</h1>
+      <p>Votre boutique de cosmétiques en ligne</p>
+    </main>
+  );
+}
