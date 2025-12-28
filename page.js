5ccbe9711ef591fe84c0a37348005c793a4eb174
bouhafsw88-cell <bouhafsw88@gmail.com>
2025-12-28 11:02:35 +0100

    Delete page.js

--- page.js
+++
@@ -1,9 +0,0 @@
-"use client";
-export default function Home() {
-  return (
-    <main>
-      <h1>Cosmicare</h1>
-      <p>Site e-commerce cosmétique – paiement CIB & livraison</p>
-    </main>
-  );
-}
