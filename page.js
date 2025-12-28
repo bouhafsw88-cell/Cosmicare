412746c9a80f3e46a2658109399d0cdbdcf85e80
bouhafsw88-cell <bouhafsw88@gmail.com>
2025-12-28 11:00:36 +0100

    Add files via upload

---
+++ page.js
@@ -0,0 +1,9 @@
+"use client";
+export default function Home() {
+  return (
+    <main>
+      <h1>Cosmicare</h1>
+      <p>Site e-commerce cosmétique – paiement CIB & livraison</p>
+    </main>
+  );
+}
