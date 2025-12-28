96cd13c205da877e58e1d650c2397fd7a7b5c54f
bouhafsw88-cell <bouhafsw88@gmail.com>
2025-12-28 12:28:44 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,29 +1,8 @@
-import ProductCard from "./components/ProductCard";
-
 export default function Home() {
   return (
-    <section className="products">
-      <h1>Nos Produits</h1>
-
-      <div className="product-grid">
-        <ProductCard
-          name="Crème Hydratante Visage"
-          price="2900"
-          image="https://via.placeholder.com/300"
-        />
-
-        <ProductCard
-          name="Sérum Vitamine C"
-          price="3500"
-          image="https://via.placeholder.com/300"
-        />
-
-        <ProductCard
-          name="Lait Corporel Nourrissant"
-          price="4200"
-          image="https://via.placeholder.com/300"
-        />
-      </div>
-    </section>
+    <main style={{ padding: "40px", textAlign: "center" }}>
+      <h1>Bienvenue sur Cosmicare ✨</h1>
+      <p>Cosmétiques luxe & accessibles en Algérie</p>
+    </main>
   );
 }
